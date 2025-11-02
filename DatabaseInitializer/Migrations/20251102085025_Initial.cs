4e5de0e0421fc5fc7ff22fedef6f35dfb19b3ce4
rafal.nowocinski <vito24222@gmail.com>
2025-11-02 09:50:48 +0100

    New db migration

---
+++ DatabaseInitializer/Migrations/20251102085025_Initial.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DatabaseInitializer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    person_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    BirthDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    Gender = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    FatherId = table.Column<int>(type: "integer", nullable: true),
+                    MotherId = table.Column<int>(type: "integer", nullable: true),
+                    SpouseId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.person_id);
+                    table.ForeignKey(
+                        name: "FK_Person_Person_FatherId",
+                        column: x => x.FatherId,
+                        principalTable: "Person",
+                        principalColumn: "person_id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Person_Person_MotherId",
+                        column: x => x.MotherId,
+                        principalTable: "Person",
+                        principalColumn: "person_id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Person_Person_SpouseId",
+                        column: x => x.SpouseId,
+                        principalTable: "Person",
+                        principalColumn: "person_id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Company",
+                columns: table => new
+                {
+                    company_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    BossId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Company", x => x.company_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    employee_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PersonId = table.Column<int>(type: "integer", nullable: false),
+                    CompanyId = table.Column<int>(type: "integer", nullable: false),
+                    ContractType = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Salary = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.employee_id);
+                    table.ForeignKey(
+                        name: "FK_Employee_Company_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Company",
+                        principalColumn: "company_id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Employee_Person_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Person",
+                        principalColumn: "person_id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Company_BossId",
+                table: "Company",
+                column: "BossId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_CompanyId",
+                table: "Employee",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_PersonId",
+                table: "Employee",
+                column: "PersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Person_FatherId",
+                table: "Person",
+                column: "FatherId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Person_MotherId",
+                table: "Person",
+                column: "MotherId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Person_SpouseId",
+                table: "Person",
+                column: "SpouseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Company_Employee_BossId",
+                table: "Company",
+                column: "BossId",
+                principalTable: "Employee",
+                principalColumn: "employee_id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Company_Employee_BossId",
+                table: "Company");
+
+            migrationBuilder.DropTable(
+                name: "Employee");
+
+            migrationBuilder.DropTable(
+                name: "Company");
+
+            migrationBuilder.DropTable(
+                name: "Person");
+        }
+    }
+}
